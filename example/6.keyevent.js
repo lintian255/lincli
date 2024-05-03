56e7e14428a6d1f8273770bcf8273d4beccc1f9d
lintian255 <lintian255@outlook.com>
2024-05-03 16:03:17 +0800

    处理键盘事件

---
+++ example/6.keyevent.js
@@ -0,0 +1,18 @@
+// 6.keyevent.js
+import process from 'node:process'
+
+if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true)
+}
+
+process.stdin.on('data', function(data) {
+    console.log('data is', data)
+    let s = data.toString('utf-8')
+    if (s === '\x03') {
+        process.exit()
+    } else if (s === '\x1b[A') {
+        console.log('up')
+    } else if (s === '\x1b[B') {
+        console.log('down')
+    }
+})
