7061ec0c443c3012434e820ba10a7c036d231895
lintian255 <lintian255@outlook.com>
2024-05-03 13:31:55 +0800

    按照 a=b 的格式解析命令行参数

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 import { argv } from 'node:process'
 
-console.log('lin cli', argv)
+let argsMapper = {}
+for (let arg of argv.slice(2)) {
+    let [k, v] = arg.split('=')
+    argsMapper[k] = v
+}
+
+// 运行 linnpm a1=b1 a2=b2 a3=b3 
+console.log('lin cli', argsMapper)
